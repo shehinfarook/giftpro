238c5ac9648afd53d48272df07a8147975b38688
Shehin farook <shehinfarook@outlook.com>
2026-04-06 16:22:36 +0530

    fix navigate

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -2,22 +2,28 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Game() {
+
   const navigate = useNavigate();
   const [pos, setPos] = useState({ x: 0, y: 0 });
 
   const moveButton = () => {
+    const maxX = window.innerWidth / 3;
+    const maxY = window.innerHeight / 3;
+
     setPos({
-      x: Math.random() * 200 - 100,
-      y: Math.random() * 200 - 100
+      x: Math.random() * maxX - maxX / 2,
+      y: Math.random() * maxY - maxY / 2,
     });
   };
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center bg-white">
+    <div className="h-screen flex flex-col items-center justify-center bg-white overflow-hidden">
 
-      <h1 className="text-xl mb-6">Do you love me? 😌</h1>
+      <h1 className="text-xl mb-6 text-center">
+        Do you love me? 😌
+      </h1>
 
-      <div className="flex gap-4 relative">
+      <div className="relative flex gap-4">
         <button
           onClick={() => navigate("/final")}
           className="bg-green-500 text-white px-6 py-2 rounded"
@@ -26,9 +32,10 @@ export default function Game() {
         </button>
 
         <button
+          onTouchStart={moveButton}   
           onMouseEnter={moveButton}
           style={{ transform: `translate(${pos.x}px, ${pos.y}px)` }}
-          className="bg-red-500 text-white px-6 py-2 rounded absolute"
+          className="bg-red-500 text-white px-6 py-2 rounded"
         >
           No 😭
         </button>
